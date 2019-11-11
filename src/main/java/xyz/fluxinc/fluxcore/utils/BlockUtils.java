db90b12a7e067a4a30d7311b863cee942e395152
Celeste Magisteel <celestemagisteel@gmail.com>
2019-11-11 14:49:08 +0000

    Implemented Corner Searching, as well as directional searching.

--- src/main/java/xyz/fluxinc/fluxcore/utils/BlockUtils.java
+++ src/main/java/xyz/fluxinc/fluxcore/utils/BlockUtils.java
@@ -6,6 +6,8 @@ import org.bukkit.block.Block;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Math.abs;
+
 public class BlockUtils {
 
     /*
@@ -15,11 +17,17 @@ public class BlockUtils {
      */
 
     public static final int TOP_FACE = 1;
+    public static final int UP_DIRECTION = 1;
     public static final int NORTH_FACE = 2;
+    public static final int NORTH_DIRECTION = 2;
     public static final int EAST_FACE = 3;
+    public static final int EAST_DIRECTION = 3;
     public static final int SOUTH_FACE = 4;
+    public static final int SOUTH_DIRECTION = 4;
     public static final int WEST_FACE = 5;
+    public static final int WEST_DIRECTION = 5;
     public static final int BOTTOM_FACE = 6;
+    public static final int DOWN_DIRECTION = 6;
 
     /**
      * Gets a list of connected blocks for the same type.
@@ -28,6 +36,17 @@ public class BlockUtils {
      * @return The list of found blocks
      */
     public static List<Block> getVMBlockList(Block startingBlock, int maxBlocks) {
+        return getVMBlockList(startingBlock, maxBlocks, false);
+    }
+
+    /**
+     * Gets a list of connected blocks for the same type.
+     * @param startingBlock The block to start checking from
+     * @param maxBlocks The maximum number of blocks to find. Use -1 for unlimited
+     * @param includeCorners should corners be included when checking VM blocks
+     * @return The list of found blocks
+     */
+    public static List<Block> getVMBlockList(Block startingBlock, int maxBlocks, boolean includeCorners) {
         List<Block> blocks = new ArrayList<>();
         Material template = startingBlock.getType();
         List<Block> toVisit = new ArrayList<>();
@@ -37,7 +56,12 @@ public class BlockUtils {
             blocks.addAll(toVisit);
             toVisit = new ArrayList<>();
             for (Block block : processQueue) {
-                List<Block> blockFaces = getBlockFaces(block);
+                List<Block> blockFaces;
+                if (includeCorners) {
+                    blockFaces = getBlockCube(block, 1);
+                } else {
+                     blockFaces = getBlockFaces(block);
+                }
                 for (Block blockFace : blockFaces) {
                     if (block.getType() == template && !toVisit.contains(blockFace) && !blocks.contains(blockFace)) {
                         toVisit.add(block);
@@ -68,9 +92,46 @@ public class BlockUtils {
     }
 
     /**
-     * Get a block at a specific face. Use constants to specify face
+     * Get a cube of blocks, given the center block
+     * @param block The starting block
+     * @param radius How many blocks away from center should be listed
+     * @return The list of blocks
+     */
+    public static List<Block> getBlockCube(Block block, int radius) {
+        List<Block> blocks = new ArrayList<>();
+
+        for (int x = abs(radius) * -1; x <= abs(radius); x++) {
+            for (int y = abs(radius) * -1; y <= abs(radius); y++) {
+                for (int z = abs(radius) * -1; z <= abs(radius); z++) {
+                    blocks.add(block.getWorld().getBlockAt(block.getX() + x, block.getY() + y, block.getZ() + z));
+                }
+            }
+        }
+        return blocks;
+    }
+
+    /**
+     * Get a list of blocks of the same type in a given direction
+     * @param block The starting block
+     * @param direction The direction to head. Use constants to specify
+     * @return
+     */
+    public static List<Block> getDirectionalBlockList(Block block, int direction) {
+        List<Block> blocks = new ArrayList<>();
+        Block nextBlock = block;
+        do {
+            blocks.add(nextBlock);
+            Block newBlock = getBlockFace(block, direction);
+            nextBlock = newBlock != null && newBlock.getType() == block.getType() ? newBlock : null;
+        } while(nextBlock != null);
+        return blocks;
+    }
+
+
+    /**
+     * Get a block at a specific face
      * @param block The block to check
-     * @param face The specific face you wish to get
+     * @param face The specific face you wish to get. Use constants to specify
      * @return The block at that face
      */
     public static Block getBlockFace(Block block, int face) {

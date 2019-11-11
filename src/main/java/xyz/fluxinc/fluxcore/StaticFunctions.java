009664e7686a4e2e774516fb067f05823ef74985
Celeste Magisteel <celestemagisteel@gmail.com>
2019-11-11 08:01:40 +0000

    Added VeinMiner block method to StaticFunctions

--- src/main/java/xyz/fluxinc/fluxcore/StaticFunctions.java
+++ src/main/java/xyz/fluxinc/fluxcore/StaticFunctions.java
@@ -1,6 +1,9 @@
 package xyz.fluxinc.fluxcore;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -30,6 +33,39 @@ public class StaticFunctions {
         return lore.replaceAll(ChatColor.COLOR_CHAR + "", "");
     }
 
+    private static List<Block> getVMBlockList(Player player, Block startingBlock, int maxBlocks) {
+        List<Block> blocks = new ArrayList<>();
+        Material template = startingBlock.getType();
+        List<Block> toVisit = new ArrayList<>();
+        toVisit.add(startingBlock);
+        while (!toVisit.isEmpty() && blocks.size() < maxBlocks) {
+            List<Block> processQueue = new ArrayList<>(toVisit);
+            blocks.addAll(toVisit);
+            toVisit = new ArrayList<>();
+            for (Block block : processQueue) {
+                List<Block> blockFaces = getBlockFaces(block);
+                for (Block blockFace : blockFaces) {
+                    if (block.getType() == template && !toVisit.contains(blockFace) && !blocks.contains(blockFace)) {
+                        toVisit.add(block);
+                    }
+                    if (blocks.size() >= maxBlocks) { break; }
+                }
+                if (blocks.size() >= maxBlocks) { break; }
+            }
+        }
+        return blocks;
+    }
+
+    private static List<Block> getBlockFaces(Block block) {
+        List<Block> blocks = new ArrayList<>();
+        blocks.add(block.getWorld().getBlockAt(block.getX()+1, block.getY(), block.getZ()));
+        blocks.add(block.getWorld().getBlockAt(block.getX()-1, block.getY(), block.getZ()));
+        blocks.add(block.getWorld().getBlockAt(block.getX(), block.getY()+1, block.getZ()));
+        blocks.add(block.getWorld().getBlockAt(block.getX(), block.getY()-1, block.getZ()));
+        blocks.add(block.getWorld().getBlockAt(block.getX(), block.getY(), block.getZ()+1));
+        blocks.add(block.getWorld().getBlockAt(block.getX(), block.getY(), block.getZ()-1));
+        return blocks;
+    }
 
 
 }

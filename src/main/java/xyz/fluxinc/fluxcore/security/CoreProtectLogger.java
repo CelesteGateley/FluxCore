776847fabe16c8e770e7307ca7d5f692a497e124
CelesteMagisteel <celestemagisteel@gmail.com#>
2019-12-27 20:59:50 +0000

    Switched CoreProtectLogger to implement static methods

--- src/main/java/xyz/fluxinc/fluxcore/security/CoreProtectLogger.java
+++ src/main/java/xyz/fluxinc/fluxcore/security/CoreProtectLogger.java
@@ -3,50 +3,47 @@ package xyz.fluxinc.fluxcore.security;
 
 import net.coreprotect.CoreProtect;
 import net.coreprotect.CoreProtectAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public class CoreProtectLogger {
 
-    private CoreProtect coreProtectInstance = null;
-    private CoreProtectAPI apiInstance = null;
-
-    /**
-     * Initialize the CoreProtectLogger
-     * @param instance The expected instance of CoreProtect
-     */
-    public CoreProtectLogger(Plugin instance) {
-        if (instance instanceof CoreProtect) {
-            coreProtectInstance = (CoreProtect) instance;
-            apiInstance = coreProtectInstance.getAPI();
-            if (!apiInstance.isEnabled() || apiInstance.APIVersion() < 6) { coreProtectInstance = null; apiInstance = null; }
-        }
-    }
-
     /**
      * Log a player breaking a block
      * @param player The player performing the action
      * @param block The block being broken
      */
-    public void logBlockBreak(Player player, Block block) {
-        if (isCoreProtectDisabled()) { return; }
-        apiInstance.logRemoval(player.getName(), block.getLocation(), block.getType(), block.getBlockData());
+    public static void logBlockBreak(Player player, Block block) {
+        CoreProtectAPI api = getCoreProtectAPI();
+        if (api != null) {
+            api.logRemoval(player.getName(), block.getLocation(), block.getType(), block.getBlockData());
+        }
     }
-
 
     /**
      * Log a player placing a block
      * @param player The player performing the action
      * @param block The block being placed
      */
-    public void logBlockPlace(Player player, Block block) {
-        if (isCoreProtectDisabled()) { return; }
-        apiInstance.logPlacement(player.getName(), block.getLocation(), block.getType(), block.getBlockData());
+    public static void logBlockPlace(Player player, Block block) {
+        CoreProtectAPI api = getCoreProtectAPI();
+        if (api != null) {
+            api.logPlacement(player.getName(), block.getLocation(), block.getType(), block.getBlockData());
+        }
     }
 
-    private boolean isCoreProtectDisabled() {
-        return coreProtectInstance == null
-                || apiInstance == null;
+
+    private static CoreProtectAPI getCoreProtectAPI() {
+        Plugin plugin = Bukkit.getPluginManager().getPlugin("CoreProtect");
+
+        if (plugin instanceof CoreProtect) {
+            CoreProtectAPI api = ((CoreProtect) plugin).getAPI();
+            if (api.isEnabled() && api.APIVersion() > 6) {
+                return api;
+            }
+        }
+        return null;
     }
 }
